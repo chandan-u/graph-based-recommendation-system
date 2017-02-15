03eed848df0416b1ebef26f0f5b46887bfcb23f9
chandan uppuluri <chandan.uppuluri@gmail.com>
2017-02-14 19:58:27 -0500

    updated READMe

--- algos.py
+++ algos.py
@@ -1,4 +1,4 @@
-
+import operator
 
 from data import load, load_movielens
 
@@ -29,14 +29,13 @@ def greedy():
         maxrating = max(row)
         print user_movie_matrix[user_movie_matrix[row] == maxrating]
 
-
+    pass
 
 
 
 
 
 def bfs_paths(graph, start, goal):
-    print "hi 2"
     flag = "product"
     queue = [(start, [start])]
     nonzero_indices = []
@@ -47,15 +46,13 @@ def bfs_paths(graph, start, goal):
            column = graph[:, [vertex]]
            nonzero_indices =  column.nonzero()
            nonzero_indices =  nonzero_indices[0]
-           print "thisisinside" 
            for child_index in nonzero_indices:
                row = graph[child_index]
                nonzero_row_indices = row.nonzero()[0]
 
                for row_index in nonzero_row_indices:
                    if goal == row_index:
-                       path = path + [child_index]
-                       print path 
+                       yield  path + [child_index]
                    else:
                        queue.append((row_index, path + [row_index]))
 
@@ -97,14 +94,19 @@ def graph_search(biparte_matrix):
         if target_vector[root] != 0 :
             bfs_roots.append(root)
 
-  
+ 
+    path =[]
+    data = {}
     for item_index in range(biparte_matrix.shape[0]):
         
         if target_vector[item_index] == 0:
             
             for root in bfs_roots:
-                bfs_paths(biparte_matrix,  root, item_index )
-
+                path.append(   bfs_paths(biparte_matrix,  root, item_index ))
+            data[item_index] = path
+            
+    
+    return data 
 def user_base_collabertive_filtering(): 
 
     
@@ -137,10 +139,25 @@ def user_base_collabertive_filtering():
         biparte_matrix = user_movie_matrix[closest_indices[0:4]] 
         # now execute the graph search for recommendation
         
-        graph_search(biparte_matrix)
-        print biparte_matrix
+        paths = graph_search(biparte_matrix)
         
+       
 
+
+        # compute the weights of paths
+        data= {}                     
+        for item in paths.keys():
+            print item
+            weight = 0   
+            allpaths = paths[item]
+            for path in allpaths:
+                depth = len(path)
+                weight = weight + (0.5)**depth
+            data[item] = weight                   
+
+        # find the which movie has great weight:
+        fav_movie = max(data.iteritems(), key=operator.itemgetter(1))[0]  
+        
 
 def get_movie_avg_rating(id, ratings):
     """
@@ -156,6 +173,28 @@ def get_movie_avg_rating(id, ratings):
         if row["movieId"] == id:
             rating = rating + row["rating"]    
     return rating
+
+
+
+def get_user_movie_rating( id, ratings, target_user):
+    """
+    return user rating of the movie
+
+    """
+
+   
+   #df = ratings.movieId == id
+
+    rating =  0
+    df = ratings[ratings.userId == 1]
+    for index, row in df.iterrows():
+        if row["movieId"] == id:
+            rating = rating + row["rating"]    
+    return rating
+
+ 
+
+
 
 def content_based_filtering():
     """
@@ -193,7 +232,7 @@ def content_based_filtering():
             movie_ids.append(row["movieId"])
     
     
-
+    # compute the average of the ratings for each genre watched by the user
     genre_dict = {}
     genre_count_dict = {}
     genre_ratio = {}
@@ -203,14 +242,14 @@ def content_based_filtering():
         for index, row in df.iterrows():
              genres = row["genres"]
              genres = genres.lower()
-             genres = genres.split('|')
+             genres = genres.split('|') 
         
-        rating = get_movie_avg_rating(id, ratings)
+        rating = get_user_movie_rating(id, ratings, target_user =1 )
 
         for genre in genres:
             if genre in genre_dict.keys():
                 genre_dict[genre] = genre_dict[genre] + rating
-                genre_count_dict = genre_count_dict + 1
+                genre_count_dict[genre] = genre_count_dict[genre] + 1
             else:
                 genre_dict[genre] = rating 
                 genre_count_dict[genre] = rating
@@ -219,32 +258,79 @@ def content_based_filtering():
         ratio  = genre_dict[key] / float(genre_count_dict[key])
         genre_dict[key] = ratio
  
-    import operator
     fav_genre = max(genre_dict.iteritems(), key=operator.itemgetter(1))[0]
      
      # get the best movies from that genre:
-
-     # get all the movies that belong to the genre:
      
     genres_ids = []
     fav_movie_id = 0
-    for index, row in movies:
+    for index, row in movies.iterrows():
         genres = row['genres']
         genres = genres.lower()
         genres = genres.split('|')
         if fav_genre in genres:
             movie_rating = get_movie_avg_rating(row["movieId"], ratings )
             if movie_rating > rating:
-               fav_movie_id = row["id"] 
-
+               fav_movie_id = row["movieId"] 
+               rating = movie_rating
 
     print "content based recommended movie is:"
     print movies[movies.movieId == fav_movie_id]
 
          
+    return fav_mov_id     
+
+
+def evaluation():
+
+    """
+       lets only compute for one target user: As it is taking too long
+    """
+    
+    fav_mov_id_con = content_based_filtering()
+
+    fav_mov_id_col = user_base_collabertive_filtering()
+
+    
+    for user in range(2):
+        distances = []
+        for anotheruser in range(user_movie_matrix.shape[0]):
+            
+            distance = np.linalg.norm(user_movie_matrix[user] - user_movie_matrix[anotheruser] )
+            distances.append(distance) 
+
+            
+        # get the similar users 
+        # indices of the similar users: closest 20
+        # non zero too
+        closest_all_indices=np.argpartition(distances, -20)[-20:]
+        closest_indices = []
+        for index in closest_all_indices:
+            if distances[index] != 0:
+                closest_indices.append(index)
+        
+        
+        
          
+        # consider the first five closest neighbors for the recommendation
+
+        closest_indices.insert(0, user) 
+        biparte_matrix = user_movie_matrix[closest_indices[0:4]] 
+      
+  
+    target_col_vector =user_movie_matrix[0]
+    target_con_vector = user_movie_matrix[0]
+   
+    score_col = 0
+    score_con = 0
+    for row in biparte_matrix: 
+        score_col = score_col +  np.linalg.norm(row, target_col_vector)
+        score_con = score_con + np.linalg.norm(row, target_con_vector)
 
 
-#user_base_collabertive_filtering()
+    print "The collaberative filtering sum of distances is: ", score_col
+    print "The contend based filtering sum of distances is: ", score_con
 
-content_based_filtering()
+
+
+evaluation()   
